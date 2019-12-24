1a4bddfadd298b1567d2a7b4b02d960b7180a157
Jawad Rizvi <jawadrizvi40@gmail.com>
2019-12-23 22:00:37 -0700

    initial files

---
+++ product-role/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TableExamplePagination from './components/TableExamplePagination.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <TableExamplePagination></TableExamplePagination>
+    </div>
+  );
+}
+
+export default App;
